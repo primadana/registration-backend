de35d8065ec9c1d3ffd640d302e1b96fcf52f54f
PRIMADANA\prima <bellaarifprimadana@gmail.com>
2019-10-12 17:31:07 +0700

    commit

--- config/web.php
+++ config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'US4suGtyg3YRTFgKL8XBH1VAdLeFMXQc',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
--- controllers/RegistrationController.php
+++ controllers/RegistrationController.php
@@ -1,12 +1,27 @@
 <?php
 
 namespace app\controllers;
+use Yii;
+use yii\helpers\Json;
+use app\models\UserRegisration;
 
 class RegistrationController extends \yii\web\Controller
 {
+    public $enableCsrfValidation = false;
+    
     public function behaviors()
     {
         $behaviors = parent::behaviors();
+
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Origin' => ['localhost', '*'],
+                'Access-Control-Request-Method' => ['POST', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Max-Age' => '600',
+            ],
+        ];
 
         $behaviors['verbs'] = [
                 'class' => \yii\filters\VerbFilter::className(),
@@ -18,13 +33,36 @@ class RegistrationController extends \yii\web\Controller
         return $behaviors;
     }
 
+    public function beforeAction($action)
+    {
+        //\Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        return parent::beforeAction($action);
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
     }
-
+    
     public function actionSaveRegistration(){
-        return 'Registration';
+        //$params = json_decode(file_get_contents('php://input'), true);
+        $params = json_decode(Yii::$app->request->getRawBody(), true);
+        // var_dump($params['data']['email']); die();
+        // return json_decode($params, true);
+        $user = new UserRegisration;
+        $user->email = $params['data']['email'];
+        $user->first_name = $params['data']['first_name'];
+        $user->last_name = $params['data']['last_name'];
+        $user->full_name = $user->first_name.' '.$user->last_name;
+        $user->date_of_birth = $params['data']['date'].'-'.$params['data']['month'].'-'.$params['data']['year'];
+        $user->gender = $params['data']['gender'];
+        $user->phone_number = $params['data']['phone_number'];
+        if($user->save()){
+            return 'true';
+        }
+        else{
+            var_dump($user->errors);die;
+        }
     }
 
 }
